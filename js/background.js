d6637f2f9406256d5528636e5a19a2b42ab341c0
Akshay S Dinesh <asdofindia@gmail.com>
2020-01-10 14:29:50 +0530

    feat: middle click to change direction

--- js/background.js
+++ js/background.js
@@ -1,14 +1,57 @@
-const handleClick = (tab) => {
+let direction = 'forward'
 
-    // see contents of tab at https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/Tab
-    const { id } = tab
+const reverseDirection = () => direction = direction === 'forward' ? 'backward' : 'forward'
 
+const iconForDirection = direction => direction === 'forward' ? 'assets/right-arrow.png' : 'assets/left-arrow.png'
+
+const selectorForDirection = direction => direction === 'forward' ? '.blog-pager-newer-link' : '.blog-pager-older-link'
+
+const setCorrectIcon = (tabId) => {
+    browser.pageAction.setIcon({
+        path: iconForDirection(direction),
+        tabId
+    })
+}
+
+const navigateAppropriately = (tabId) => {
     const script = {
-        code: `document.querySelector('.blog-pager-newer-link').click()`
+        code: `document.querySelector('${selectorForDirection(direction)}').click()`
     }
     browser.tabs.executeScript(
-        id,
+        tabId,
         script
     )
 }
+
+const handleClick = (tab, {button}) => {
+    const { id } = tab
+    // see contents of tab at https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/Tab
+
+    if (button === 1) {
+        // middle click is to change direction
+
+        reverseDirection()
+        setCorrectIcon(id)
+
+    } else {
+        // normal click
+
+        navigateAppropriately(id)
+
+    }
+}
+
 browser.pageAction.onClicked.addListener(handleClick);
+
+const handleTabUpdate = (tabId, changeInfo) => {
+    if (changeInfo.status !== 'loading') return;
+    setCorrectIcon(tabId)
+}
+
+browser.tabs.onUpdated.addListener(handleTabUpdate);
+
+const handleTabActive = ({tabId}) => {
+    setCorrectIcon(tabId)
+}
+
+browser.tabs.onActivated.addListener(handleTabActive);
